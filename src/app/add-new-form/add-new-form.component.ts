1e0e3d685d082e2665d9042782e6ecd02e0b6dfb
Bhanukakumara <bhanukarasika@gmail.com>
2025-04-29 14:31:19 +0530

    add new emplyee

--- src/app/add-new-form/add-new-form.component.ts
+++ src/app/add-new-form/add-new-form.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { EmployeeService, Employee } from '../employee.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-add-new-form',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './add-new-form.component.html',
   styleUrl: './add-new-form.component.css'
 })
-export class AddNewFormComponent {
-  addEmployee(){
-    
+export class AddNewFormComponent implements OnInit{
+  employees: Employee[] = [];
+  newEmployee: Employee = {
+    name: '',
+    email: '',
+    department: '',
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString()
+  };
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+    this.loadEmployees();
+  }
+
+  loadEmployees() {
+    this.employeeService.getEmployees().subscribe(data => {
+      this.employees = data;
+    });
+  }
+
+  onSubmit() {
+    this.newEmployee.createdAt = new Date().toISOString();
+    this.newEmployee.updatedAt = new Date().toISOString();
+    this.employeeService.addEmployee(this.newEmployee).subscribe(() => {
+      this.loadEmployees();
+      this.newEmployee = {
+        name: '',
+        email: '',
+        department: '',
+        createdAt: '',
+        updatedAt: ''
+      };
+    });
+  }
+
+  deleteEmployee(id: number) {
+    if (confirm("Are you sure you want to delete?")) {
+      this.employeeService.deleteEmployee(id).subscribe(() => {
+        this.loadEmployees();
+      });
+    }
   }
 }
